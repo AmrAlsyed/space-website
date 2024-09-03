a1751c3e77c542a51fbf37a1fb1d746c6ed13d60
AmrAlsyed <Xaxeth@gmail.com>
2024-09-04 01:50:04 +0300

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
